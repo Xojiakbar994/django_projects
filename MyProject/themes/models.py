bf9d03072063da546cd1dd71ae211316da39fe39
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2023-09-06 21:08:52 +0500

    orm practice

--- MyProject/themes/models.py
+++ MyProject/themes/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Lessons(models.Model):
-    lesson_title = models.CharField(max_length=100)
+class Subjects(models.Model):
+    subject_name = models.CharField(max_length=50, verbose_name='Subject')
 
     def __str__(self):
-        return self.lesson_title
+        return self.subject_name
+
+
+class Teachers(models.Model):
+    teacher_fio = models.CharField(max_length=50, verbose_name='Teacher Fio')
+
+    def __str__(self):
+        return self.teacher_fio
+
+
+class Lessons(models.Model):
+    lesson_title = models.CharField(max_length=100, verbose_name='Lesson title')
+    lesson_subject = models.ForeignKey(Subjects, on_delete=models.CASCADE, verbose_name='Subject ID', null=True)
+    lesson_teacher = models.ManyToManyField(Teachers, verbose_name='Teachers ID', null=True)
+    lesson_date = models.DateField(verbose_name='Lesson date', auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.lesson_title, self.lesson_subject, self.lesson_teacher, self.lesson_date
+    
+    def get(self):
+        return Lessons.objects.all()
